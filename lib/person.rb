097bac688a305e7da4ef7ded9d935b5385ee1d19
Ethan Lee <wxliyc@gmail.com>
2022-04-06 11:06:45 -0500

    commit tree e8e3e9bb25604bf7fa922efc9ea5a0e63109eb3e

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,20 @@
 class Person
-  # your code here
+  attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_width, :pant_length
+  
+  def initialize(attr)
+    @name = attr[:name]
+    @birthday = attr[:birthday]
+    @hair_color = attr[:hair_color]
+    @eye_color = attr[:eye_color]
+    @height = attr[:height]
+    @weight = attr[:weight]
+    @handed = attr[:handed]
+    @complexion = attr[:complexion]
+    @t_shirt_size = attr[:t_shirt_size]
+    @wrist_size = attr[:wrist_size]
+    @glove_size = attr[:glove_size]
+    @pant_length = attr[:pant_length]
+    @pant_width = attr[:pant_width]
+  end
+
 end
